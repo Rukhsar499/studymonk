3a5aa20bc1ef653711cbd7834624f58f33e41246
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2025-08-09 11:32:42 +0530

    hii

--- src/app/BootstrapClient.tsx
+++ src/app/BootstrapClient.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useEffect } from "react";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 export default function BootstrapClient() {
-  // no need for useEffect here if just importing bootstrap js globally
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return null;
 }
